2e1dfc581155c9b6443c2496de712da365b32f19
Sibin T S <62069255+Sibints2000@users.noreply.github.com>
2021-07-31 08:27:06 +0000

    Tenth Commit

--- main.py
+++ main.py
@@ -12,3 +12,4 @@ people = int(input("How many people to split the bill? "))
 tip_as_percent = tip / 100
 total_tip_amount = bill * tip_as_percent
 total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
