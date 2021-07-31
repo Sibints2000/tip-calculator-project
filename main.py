848fb143137870d0691dc763aec46947ea929ecd
Sibin T S <62069255+Sibints2000@users.noreply.github.com>
2021-07-31 08:26:09 +0000

    Ninth Commit

--- main.py
+++ main.py
@@ -11,3 +11,4 @@ tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
 people = int(input("How many people to split the bill? "))
 tip_as_percent = tip / 100
 total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
